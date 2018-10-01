e87c4d16e70c84dc7fe8054f819e42e1a24c80c0
Matthew Peveler <matt.peveler@gmail.com>
2018-10-01 13:39:40 -0400

    Update files to match eslint style

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 A node.js implementation of a ring buffer for binary data (using the builtin Buffer)
-Not thread safe/asynchronous 
+Not thread safe/asynchronous
 */
 class BinaryRingBuffer {
   constructor(capacity) {
@@ -8,12 +8,11 @@ class BinaryRingBuffer {
     this.buffer = Buffer.alloc(capacity);
     this.buffer.fill(0);
     this.numItems = 0;
-    
+
     // Points to the index where the next index will be read/removed
     this.head = 0;
     // Points to the index where the next item will be entered
     this.tail = 0;
-
   }
 
   get length() {
@@ -22,12 +21,12 @@ class BinaryRingBuffer {
 
   // returns the next index in the ring
   // handles wrapping
-  incrementPtr(ptr){
+  incrementPtr(ptr) {
     return (ptr + 1) % this.capacity;
   }
 
   // For debugging
-  toString(){
+  toString() {
     return this.buffer.toString();
   }
 
@@ -35,21 +34,20 @@ class BinaryRingBuffer {
   // If n goes past the tail, only return data up to the tail
   // O(n) on the given number n
   read(n) {
-
     // check for empty buffer
-    if(this.numItems === 0){
+    if (this.numItems === 0) {
       return Buffer.alloc(0);
     }
 
     // find the distance from head to tail
     let headTailDist = 0;
-    if(this.tail < this.head){
+    if (this.tail < this.head) {
       headTailDist = this.capacity - this.head + this.tail;
     }
-    else if(this.tail === this.head){
+    else if (this.tail === this.head) {
       headTailDist = this.capacity;
     }
-    else{
+    else {
       headTailDist = this.tail - this.head;
     }
 
@@ -59,50 +57,38 @@ class BinaryRingBuffer {
     let readBuff = Buffer.alloc(readBuffSize);
 
     // loop until we hit data length
-    for(let i = 0; i < n; ++i){
-      
+    for (let i = 0; i < n; ++i) {
       readBuff[i] = this.buffer[this.head]; 
       --this.numItems;
       this.head = this.incrementPtr(this.head);
 
       // check if we've reached the tail
-      if(this.head === this.tail){
+      if (this.head === this.tail) {
         return readBuff;
       }
-
     } 
 
     return readBuff;
-
   }
-
-
 
   // Writes the data to the buffer. Will override old data if capacity is hit
   // O(n) on the length of the data
   write(data) {
-
-    for(let i = 0; i < data.length; ++i){
-
+    for (let i = 0; i < data.length; ++i) {
       // write the data to the tail
       this.buffer[this.tail] = data[i];
 
       this.tail = this.incrementPtr(this.tail);
 
       // if the buffer is at capacity, move foward the head
-      if(this.numItems === this.capacity){
+      if (this.numItems === this.capacity) {
         this.head = this.incrementPtr(this.head);
       }
-      else{
+      else {
         ++this.numItems;  
       }
-
     }
-
   }
-
 }
 
-
 module.exports = BinaryRingBuffer;
-
